e2fbdf102e2b1064189a22e7a564d41cf6c42b49
ventt <rfhfcm2009@gmail.com>
2025-03-06 01:05:38 +0300

    git commit git push

---
+++ app/src/main/java/com/example/fizmind/Activity/AboutUsActivity.java
@@ -0,0 +1,13 @@
+package com.example.fizmind.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.fizmind.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
---
+++ app/src/main/java/com/example/fizmind/Activity/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.fizmind.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.fizmind.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
