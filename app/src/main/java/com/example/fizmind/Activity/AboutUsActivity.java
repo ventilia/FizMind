ad6aa73d1afd83dcd4d42b803442dc3b5924092c
ventt <rfhfcm2009@gmail.com>
2025-03-14 01:53:01 +0300

    git commit git push

--- app/src/main/java/com/example/fizmind/Activity/AboutUsActivity.java
+++ app/src/main/java/com/example/fizmind/Activity/AboutUsActivity.java
@@ -1,7 +1,14 @@
 package com.example.fizmind.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.example.fizmind.R;
 
 public class AboutUsActivity extends AppCompatActivity {
@@ -9,5 +16,25 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        // Настройка кнопки "назад"
+        ImageView backArrow = findViewById(R.id.backButton);
+        backArrow.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("open_drawer", true); // Флаг для открытия DrawerLayout
+            startActivity(intent);
+            finish();
+        });
+
+        // Настройка аккордеона
+        TextView accordionHeader = findViewById(R.id.accordion_header);
+        LinearLayout accordionContent = findViewById(R.id.accordion_content);
+        accordionHeader.setOnClickListener(v -> {
+            if (accordionContent.getVisibility() == View.GONE) {
+                accordionContent.setVisibility(View.VISIBLE);
+            } else {
+                accordionContent.setVisibility(View.GONE);
+            }
+        });
     }
 }
