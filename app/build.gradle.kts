816ac5f9f1a4e7e49c64c909e5caaa3ddaf28a51
ventt <rfhfcm2009@gmail.com>
2025-05-03 23:13:42 +0300

    git commit git push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,14 +32,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.preference)
-    implementation(libs.room.common.jvm)
-    implementation(libs.room.runtime.android)
+
+// Room dependencies
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
